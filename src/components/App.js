2098efa6b30884c868b569324f59df0cd11a9d5c
lalmastar <107454522+lalmastar@users.noreply.github.com>
2023-01-22 19:34:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
+import Task from "./Task";
 
-function App() 
-{
+function App() {
+	const [text,setText] = useState('');
+	const [tasklist,setTasklist] = useState([]);
+
+	const addTask = ()=> {
+		if(!text) return;
+		let newtask = {id:Date.now(),text}
+		setTasklist([...tasklist,newtask])
+		setText('');
+	}
+	const deleteTask = (id)=> {
+		let newList = tasklist.filter(task=>task.id !== id);
+		setTasklist(newList);
+	}
+	const editTask = (id,text)=> {
+		const newList = [...tasklist];
+		let task = newList.find(task=>task.id===id);
+		task.text = text;
+		setTasklist(newList);
+	}
+
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+		<textarea id="task" cols="30" rows="10" value={text} onChange={(e)=>setText(e.target.value)}></textarea>
+		<br />
+		<button id="btn" onClick={addTask}>Add Task</button>
+		<ul>
+			{tasklist.map(task=>(<Task task={task} key={task.id} editTask={editTask} deleteTask={deleteTask}/>))}
+		</ul>
 	</div>
 	);
 }
